9b30ad376b37429a6df91c119d6969e312972994
razvanr91 <razvanroman1991@gmail.com>
2021-09-19 00:48:31 +0100

    Create and add navbar component + images for activity types

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { Container, Header, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get<Activity[]>("https://localhost:5001/api/activities").then((response) => {
-      setActivities(response.data);
-    });
+    axios
+      .get<Activity[]>("https://localhost:5001/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
   }, []);
 
   return (
-    <div>
-      <Header as="h2" icon="users" content="Reaction" />
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id} content={activity.title} />
-        ))}
-      </List>
-    </div>
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id} content={activity.title} />
+          ))}
+        </List>
+      </Container>
+    </Fragment>
   );
 }
 
