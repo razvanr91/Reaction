f1a9aa5ad72da83b83f037d6bf1efa445956681d
razvanr91 <razvanroman1991@gmail.com>
2021-09-20 13:02:24 +0100

    Add loading indicator

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,15 +1,32 @@
 import axios, { AxiosResponse } from "axios";
 import { Activity } from "../models/activity";
 
+const sleep = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
+
 axios.defaults.baseURL = "https://localhost:5001/api";
+
+axios.interceptors.response.use((response) => {
+  return sleep(1000)
+    .then(() => {
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+      return Promise.reject(error);
+    });
+});
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-  post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
-  put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-  del: <T> (url: string) => axios.delete<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
 
 const Activities = {
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,13 +5,13 @@ import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { v4 as uuid } from "uuid";
 import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<
-    Activity | undefined
-  >(undefined);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     agent.Activities.list().then((response) => {
@@ -21,6 +21,7 @@ function App() {
         activities.push(activity);
       });
       setActivities(activities);
+      setLoading(false);
     });
   }, []);
 
@@ -55,6 +56,7 @@ function App() {
   function handleDeleteActivity(id: string) {
     setActivities([...activities.filter((x) => x.id !== id)]);
   }
+  if(loading) return <LoadingComponent content='Loading' />
 
   return (
     <Fragment>
