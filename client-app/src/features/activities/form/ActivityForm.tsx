f4e81872ecd3c80ba729c2985b124ad2b5cbb55e
razvanr91 <razvanroman1991@gmail.com>
2021-09-21 14:46:54 +0100

    Refactor app to use MobX

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,20 +1,14 @@
+import { observer } from "mobx-react-lite";
 import React, { ChangeEvent, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 
-interface Props {
-  activity: Activity | undefined;
-  closeForm: () => void;
-  createOrEdit: (activity: Activity) => void;
-  submitting: boolean;
-}
 
-export default function ActivityForm({
-  activity: selectedActivity,
-  closeForm,
-  createOrEdit,
-  submitting,
-}: Props) {
+export default observer (function ActivityForm() {
+  const {activityStore} = useStore();
+
+  const{ selectedActivity, closeForm, createActivity, updateActivity, loading } = activityStore;
+
   const initialState = selectedActivity ?? {
     id: "",
     title: "",
@@ -28,7 +22,7 @@ export default function ActivityForm({
   const [activity, setActivity] = useState(initialState);
 
   function handleSubmit() {
-    createOrEdit(activity);
+    activity.id ? updateActivity(activity) : createActivity(activity);
   }
 
   function handleInputChange(
@@ -79,7 +73,7 @@ export default function ActivityForm({
           onChange={handleInputChange}
         />
         <Button
-          loading={submitting}
+          loading={loading}
           floated="right"
           positive
           type="submit"
@@ -96,4 +90,4 @@ export default function ActivityForm({
       </Form>
     </Segment>
   );
-}
+})
