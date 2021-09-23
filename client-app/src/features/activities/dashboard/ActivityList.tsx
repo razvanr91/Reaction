bbd289b4c624b4628bb42b8273f7457820daf695
razvanr91 <razvanroman1991@gmail.com>
2021-09-23 16:28:00 +0100

    Eliminate unused imports

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
 import { observer } from "mobx-react-lite";
-import { Header, Item, Segment } from "semantic-ui-react";
+import { Header} from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import ActivityListItem from './ActivityListItem';
 
 export default observer(function ActivityList() {
   const { activityStore } = useStore();
-  const {deleteActivity, groupedActivities} = activityStore;
+  const { groupedActivities} = activityStore;
 
   return (
     <Fragment>
