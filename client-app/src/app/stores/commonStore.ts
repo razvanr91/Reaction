17220372ca8bb3dc0caaf7a68cd089e6e9f7b947
razvanr91 <razvanroman1991@gmail.com>
2021-09-29 12:49:31 +0100

    Set up token upon login

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -5,6 +5,8 @@ import { StoreContext } from "./store";
 
 export default class CommonStore {
     error: ServerError | null = null;
+    token: string | null = null;
+    appLoaded = false;
 
     constructor() {
         makeAutoObservable(this);
@@ -12,5 +14,14 @@ export default class CommonStore {
 
     setServerError = (error: ServerError) => {
         this.error = error;
+    }
+
+    setToken = (token: string | null) => {
+        if(token) window.localStorage.setItem('jwt', token);
+        this.token = token;
+    }
+
+    setAppLoaded = () => {
+        this.appLoaded = true;
     }
 }
