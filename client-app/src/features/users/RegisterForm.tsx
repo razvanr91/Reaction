c06530d24add83ba4fb31fd1eeae7414da6ea4fe
razvanr91 <razvanroman1991@gmail.com>
2021-09-30 14:51:22 +0100

    Handle validation errors in the register form

--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -5,6 +5,7 @@ import { Button, Header, Label } from "semantic-ui-react";
 import MyTextInput from "../../app/common/form/MyTextInput";
 import { useStore } from "../../app/stores/store";
 import * as Yup from 'yup';
+import ValidationErrors from "../errors/ValidationErrors";
 
 export default observer(function RegisterForm() {
     const {userStore} = useStore();
@@ -13,7 +14,7 @@ export default observer(function RegisterForm() {
         <Formik
             initialValues={{displayName: '', username: '', email: '', password: '', error: null}}
             onSubmit={(values, {setErrors}) => userStore.register(values).catch(error =>
-                setErrors({error: 'Invalid email or password'}))}
+                setErrors({error}))}
             validationSchema={Yup.object({
                 displayName: Yup.string().required(),
                 email: Yup.string().required().email(),
@@ -22,14 +23,14 @@ export default observer(function RegisterForm() {
             })}
         >
             {({handleSubmit, isSubmitting, errors, isValid, dirty}) => (
-                <Form className='ui form' onSubmit={handleSubmit} autoComplete='off' >
+                <Form className='ui form error' onSubmit={handleSubmit} autoComplete='off' >
                     <Header as='h2' content='Sign up to Reaction' color='teal' textAlign='center' />
                     <MyTextInput name='displayName' placeholder='Display Name' />
                     <MyTextInput name='username' placeholder='Username' />
                     <MyTextInput name='email' placeholder='Email' />
                     <MyTextInput name='password' placeholder='Password' type='password' />
                     <ErrorMessage name='error' render ={() =>
-                        <Label style={{marginBottom: 10}} basic color='red' content={errors.error} />} />
+                        <ValidationErrors errors={errors.error} />} />
                     <Button disabled={!isValid || !dirty || isSubmitting} loading={isSubmitting} positive content='Register' type='submit' fluid />
                 </Form>
             )}
