d359927c341bc7e218a7a5d1c8d438c7d72ffa47
razvanr91 <razvanroman1991@gmail.com>
2021-10-01 15:38:45 +0100

    Load related data for activities

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Application.Activities;
 using AutoMapper;
 using Domain;
 
@@ -8,6 +10,15 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(d => d.HostUsername, options => options.MapFrom(source => source.Attendees
+                .FirstOrDefault(x => x.IsHost).AppUser.UserName));
+
+            CreateMap<ActivityAttendee, Profiles.Profile>()
+                .ForMember(d => d.DisplayName, options => options.MapFrom(source => source.AppUser.DisplayName))
+                .ForMember(d => d.Username, options => options.MapFrom(source => source.AppUser.UserName))
+                .ForMember(d => d.Bio, options => options.MapFrom(source => source.AppUser.Bio));
         }
     }
 }
