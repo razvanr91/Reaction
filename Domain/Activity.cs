3b1fdb2c58f6688d1c41ea8e964ac90038812836
razvanr91 <razvanroman1991@gmail.com>
2021-10-02 21:10:28 +0100

    Update db to add is host option and add the attendance handler

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -19,6 +19,10 @@ namespace Domain
 
         public string Venue { get; set; }
 
+        public bool IsCancelled { get; set; }
+        
+        
+
         public ICollection<ActivityAttendee> Attendees {get; set;} = new List<ActivityAttendee>();
     }
 }
